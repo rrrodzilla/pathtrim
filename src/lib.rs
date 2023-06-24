4a96fe4bd366fafebcdf380392a34e15eb84c5e8
Roland Rodriguez <lrodrrol@amazon.com>
2023-06-23 19:22:20 -0600

    fix: remove doctest for now

--- src/lib.rs
+++ src/lib.rs
@@ -9,34 +9,6 @@
 //!
 //! # Usage
 //!
-//! ```
-//!    use std::path::Path;
-//!    // at the top of your source file
-//!    use pathtrim::TrimmablePath;
-//!
-//!    // TrimmablePath is automatically implemented for all std::path::Paths in scope
-//!    let path = Path::new("/usr/local/bin/");
-//!
-//!    let trimmed = path.trim_to_nth(1).unwrap();
-//!    assert_eq!(trimmed.to_str().unwrap(), "bin");
-//!
-//!    let trimmed = path.trim_to_nth(2).unwrap();
-//!    assert_eq!(trimmed.to_str().unwrap(), "local/bin");
-//!
-//!    let trimmed = path.trim_to_nth(3).unwrap();
-//!    assert_eq!(trimmed.to_str().unwrap(), "usr/local/bin");
-//!
-//!    let trimmed = path.trim_to_nth(4);
-//!    assert!(trimmed.is_none());
-//!
-//!    let trimmed = path.trim_to_nth(300);
-//!    assert!(trimmed.is_none());
-//!
-//!    let trimmed = path.trim_to_nth(2000);
-//!    assert!(trimmed.is_none());
-//!    
-//! ```
-
 use std::path::{Component, Path, PathBuf, MAIN_SEPARATOR};
 
 /// The TrimmablePath trait on std::path::Path so you can easily obtain the
