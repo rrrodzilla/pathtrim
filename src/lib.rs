4ced091b8c03d546c1250745d3d3e934acf6babe
Roland Rodriguez <lrodrrol@amazon.com>
2023-06-23 18:38:41 -0600

    feat: add cross-platform support for TrimmablePath trait
    - Modify TrimmablePath trait to return owned PathBuf instead of &Path
    - Adjust code to handle both Unix-like systems and Windows paths
    - Update test cases to include Windows-specific tests
    
    BREAKING CHANGE: TrimmablePath trait now returns PathBuf instead of &Path

--- src/lib.rs
+++ src/lib.rs
@@ -37,25 +37,26 @@
 //!    
 //! ```
 
-use std::path::Path;
+
+use std::path::{Path, PathBuf, MAIN_SEPARATOR};
 
 /// The TrimmablePath trait on std::path::Path so you can easily obtain the
 /// last *n* parts of anything that implements AsRef<Path>.
 pub trait TrimmablePath: AsRef<Path> {
-    /// Returns an Option<&Path>.
+    /// Returns an Option<PathBuf>.
     /// If *n* is longer than the length of the Path, returns None
-    ///
-    /// Algorithm inspired by @nnethercote in the Zulip Rust channel:
-    ///
-    /// ![image](https://user-images.githubusercontent.com/24578097/145341121-1e858f4b-5ab9-436c-bcc4-9ee6effa6340.png)
-    ///
-    fn trim_to_nth(&self, n: usize) -> Option<&Path> {
+    fn trim_to_nth(&self, n: usize) -> Option<PathBuf> {
         let path = self.as_ref();
         let len = path.components().count();
+        
         if len > n {
             let mut c = path.components();
             c.nth(len - (n + 1));
-            Some(c.as_path())
+            let mut trimmed = c.as_path().to_owned();
+            if trimmed.as_os_str().is_empty() {
+                trimmed = PathBuf::from(MAIN_SEPARATOR.to_string());
+            }
+            Some(trimmed)
         } else {
             None
         }
@@ -81,6 +82,20 @@ mod tests {
         let trimmed = p.trim_to_nth(3).unwrap();
         assert_eq!(trimmed.to_str().unwrap(), "usr/local/bin");
         let trimmed = p.trim_to_nth(300);
+        assert!(trimmed.is_none());
+    }
+
+    #[cfg(windows)]
+    #[test]
+    fn it_works_on_windows() {
+        let p = Path::new(r"C:\Program Files\package\bin\");
+        let trimmed = p.trim_to_nth(1).unwrap();
+        assert_eq!(trimmed.to_str().unwrap(), "bin");
+        let trimmed = p.trim_to_nth(2).unwrap();
+        assert_eq!(trimmed.to_str().unwrap(), r"package\bin");
+        let trimmed = p.trim_to_nth(3).unwrap();
+        assert_eq!(trimmed.to_str().unwrap(), r"Program Files\package\bin");
+        let trimmed = p.trim_to_nth(4);
         assert!(trimmed.is_none());
     }
 }
